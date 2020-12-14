7ac16c8a8fe03d3af8244b59004673a8e53b0aef
AnamariaL <32013978+AnamariaL@users.noreply.github.com>
2020-12-15 01:19:31 +0200

    Update GameFrame.java

--- SnakeG/src/GameFrame.java
+++ SnakeG/src/GameFrame.java
@@ -1,13 +1,15 @@
 import javax.swing.JFrame;
 
-public class GameFrame extends JFrame{
+public class GameFrame  extends JFrame{
 	
 	GameFrame(){
 		
 		this.add(new GamePanel());
-		this.setTitle("SnakeGame");
+		this.setTitle("Snake");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		this.setResizable(false);
+		
 		this.pack();
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
